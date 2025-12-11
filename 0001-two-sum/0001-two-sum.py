cfb78aea6d74ababb8ed5f3b9940212e48d53116
Raul Lopez <151303349+raullopez33@users.noreply.github.com>
2025-12-11 10:08:45 -0800

    Time: 0 ms (100%), Space: 18.8 MB (63.33%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,31 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        '''
+        h_m = num : indices
+        
+        loop through nums
+        new_target = target - nums[i]
+
+        if new_target in the h_m 
+            return [i, h_m[new_target]]
+        
+        assume each input have exactly one solution then we can just keep that return value
+        as is 
+        '''
+
+        h_m = {}
+
+        for i in range(len(nums)):
+            new_pos = target - nums[i]
+            if new_pos in h_m:
+                return [i, h_m[new_pos]]
+            h_m[nums[i]] = i
+        
+
+
+
+
+            
+
+            
