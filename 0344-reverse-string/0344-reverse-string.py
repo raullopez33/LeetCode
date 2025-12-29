f759c57cfb3f5d9d10e9e983d6172cb744b5199d
Raul Lopez <151303349+raullopez33@users.noreply.github.com>
2025-12-28 23:18:57 -0800

    Time: 4 ms (38.77%), Space: 19.9 MB (35.7%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -7,10 +7,7 @@ class Solution(object):
         l, r = 0, len(s) - 1 
 
         while(l<r):
-            tmp = s[l]
-            s[l] = s[r]
-            s[r] = tmp
-
+            s[l], s[r] = s[r], s[l]
             l += 1 
             r -= 1 
         return s
